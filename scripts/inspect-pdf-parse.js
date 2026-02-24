aa33fe0ab94633e2bd9a374de048d28a946e3235
lucidio <lucidio2010@gmail.com>
2026-02-24 18:40:58 -0400

    Actualizacion: nuevos documentos, knowledge-base modificada, archivos obsoletos eliminados

--- scripts/inspect-pdf-parse.js
+++
@@ -1,5 +0,0 @@
-const { PDFParse } = require('pdf-parse');
-
-console.log('PDFParse static properties:', Object.keys(PDFParse));
-console.log('PDFParse.setWorker exists?', typeof PDFParse.setWorker);
-console.log('PDFParse.disableWorker exists?', typeof PDFParse.disableWorker);
